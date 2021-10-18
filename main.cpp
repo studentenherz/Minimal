ccb8d6497ed01e0ba420e507792ebb284531f16d
Michel Romero <studentenherz@gmail.com>
2021-10-17 23:26:13 -0300

    added a util.hpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,29 @@
 #include <iostream>
-#include <boost/numeric/odeint.hpp>
+
+#include "util.hpp"
 
 using namespace std;
-using namespace boost::numeric::odeint;
 
-typedef boost::array<double, 6> state_type;			// state given by {rho, theta, z, v_rho, v_theta, v_z}
-typedef boost::array<double, 3> vector_type;		// a 3 coordinates vector
-
-vector_type F(const state_type &x, const double /* t */){ // Force field
-	return {0, 0, 1};
-}
 
 const double m = 1;
 
-void motion_eq(const state_type &x, state_type &dxdt, const double t ){
-	dxdt[0] = x[3];																						// d(rho)/dt = v_rho
-	dxdt[1] = x[4];																						// d(theta)/dt = v_theta
-	dxdt[2] = x[5];																						// dz/dt = v_z
-	dxdt[3] = F(x, t)[1] / m + x[0] * x[4] * x[4];							// v_rho
-	dxdt[3] = (- F(x, t)[2] / m +  2 * x[3] * x[4]) / x[0];		// v_theta
-	dxdt[5] = F(x, t)[2];																			// v_z
+// The force field
+vector_type force(const state_type &x, const double /* t */){
+	return {0, 0, 1};
 }
 
+// The observer
 void obs_cout(const state_type &x, const double t){
 	cout << t << '\t' << x[0] << ' ' << x[1] << ' ' << x[2] << ' ' << x[3] << ' ' << x[4] << ' ' << x[5] << '\n';
 }
 
 int main(int argc, char* argv[]){
 
-	state_type x = {1, 0, 0, 0, 0, 0};
+	state_type x = {1, 0, 0, 0, 0, 0}; // initial state
 
-	integrate(motion_eq, x, 0.0, 10.0, 0.01, obs_cout);
+	MotionEquationCylindrical motion_eq(force); // motion equation using the 'force'
+
+	integrate(motion_eq, x, 0.0, 10.0, 0.01, obs_cout); // integration
 
 	return 0;
 }
