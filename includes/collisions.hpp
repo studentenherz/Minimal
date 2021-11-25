cbd33342abdc93230d06f4dff5a621604d96bdea
Michel Romero <studentenherz@gmail.com>
2021-11-25 17:12:07 -0300

    fixed test/sd values and added dispersion

--- includes/collisions.hpp
+++ includes/collisions.hpp
@@ -3,9 +3,11 @@
 
 #define sqr(a) a * a
 
-#include "types.hpp"
 #include <vector>
 #include <cmath>
+
+#include "types.hpp"
+#include "util.hpp"
 
 const double two_over_sqrt_pi = 2 / sqrt(M_PI);
 
@@ -13,21 +15,32 @@ double erf_minus_d_erf(double x){
 	return (erf(x) - x * two_over_sqrt_pi * exp(- sqr(x)));
 }
 
+double G(double x){
+	return erf_minus_d_erf(x)/(2 * sqr(x));
+}
+
 class Collisions{
 	vector<int> q; // charges
 	vector<double> m; // masses
 	vector<double> logl; // log of Lambda
-	vector<scalar_field_type> T; // temperatures
+	vector<scalar_field_type> T; // temperatures (actaually velocity, not temperature)
 	vector<scalar_field_type> n; // concentrations
 	double eta; // adimensional constant
 	double m_a; // mass of test particle
 	double q_a; // charge of test particle
+
+	NormalRand ran; // random generator
 public:
-	Collisions (vector<int> q_, vector<double> m_, vector<double> logl_, vector<scalar_field_type> T_, vector<scalar_field_type> n_, double eta_, double ma, double qa): q(q_), m(m_), logl(logl_), T(T_), n(n_), eta(eta_), m_a(ma), q_a(qa) {
+	Collisions (vector<int> q_, vector<double> m_, vector<double> logl_, vector<scalar_field_type> T_, vector<scalar_field_type> n_, double eta_, double ma, double qa, NormalRand ran_): q(q_), m(m_), logl(logl_), T(T_), n(n_), eta(eta_), m_a(ma), q_a(qa), ran(ran_) {
+		if(!(q.size() == m.size() && q.size() == T.size() && q.size() == n.size()))
+			throw length_error("Los vectores deben tener la misma cantidad de elementos, uno para cada especie en orden");
+	}
+	Collisions (vector<int> q_, vector<double> m_, vector<double> logl_, vector<scalar_field_type> T_, vector<scalar_field_type> n_, double eta_, double ma, double qa): q(q_), m(m_), logl(logl_), T(T_), n(n_), eta(eta_), m_a(ma), q_a(qa), ran(1LL){
 		if(!(q.size() == m.size() && q.size() == T.size() && q.size() == n.size()))
 			throw length_error("Los vectores deben tener la misma cantidad de elementos, uno para cada especie en orden");
 	}
 
+	// Deterministic slowing down rate
 	vector_type slow_down(const state_type& x, double t){
 		vector_type r = get_position(x);
 		vector_type v = get_velocity(x);
@@ -46,6 +59,67 @@ public:
 		vector_type sd = - nu * v;
 		return sd;
 	}
+
+	double parallel_dispersion_coeff(const state_type& x, double t){
+		vector_type r = get_position(x);
+		vector_type v = get_velocity(x);
+		double v_mod = mod(v);
+
+		double nu = 0;
+		for(int i=0; i<(int)q.size(); ++i){
+			double xb = v_mod / T[i](r, t);
+			// terms that depend on plasma species
+			nu +=  sqr(q[i]) *  n[i](r, t) * logl[i] * G(xb) ;
+		}
+
+		// other terms
+		nu *= 2 * eta * sqr(q_a) / (pow(v_mod, 3) * sqr(m_a));
+		return nu;
+	}
+
+	double perpendicular_dispersion_coeff(const state_type& x, double t){
+		vector_type r = get_position(x);
+		vector_type v = get_velocity(x);
+		double v_mod = mod(v);
+
+		double nu = 0;
+		for(int i=0; i<(int)q.size(); ++i){
+			double xb = v_mod / T[i](r, t);
+			// terms that depend on plasma species
+			nu +=  sqr(q[i]) *  n[i](r, t) * logl[i] * (erf(xb) - G(xb)) ;
+		}
+
+		// other terms
+		nu *= 2 * eta * sqr(q_a) / (pow(v_mod, 3) * sqr(m_a));
+		return nu;
+	}
+
+	// Stochastic dispersion
+	vector_type dispersion(const state_type& x, double t){
+		vector_type v = get_velocity(x);
+		double v_mod = mod(v);
+
+		// nu coefficients
+		double nu_parallel = parallel_dispersion_coeff(x, t);
+		double nu_perpendicular = parallel_dispersion_coeff(x, t);
+
+		// parallel and perpendicular versors
+		vector_type e_par = v / v_mod;
+		vector_type e_perp_1;
+
+		do{
+			vector_type random_vetor = {ran(), ran(), ran()};
+			e_perp_1 = cross(random_vetor, e_par);
+		}while(dot(e_par, e_perp_1) != 0); // get first perpendicular vector
+
+		e_perp_1 = e_perp_1 / mod(e_perp_1); // normalize
+		vector_type e_perp_2 = cross(e_perp_1, e_par); // second perpendicular versor
+
+		vector_type dvdt = sqrt(nu_parallel) * v_mod * ran() * e_par + sqrt(nu_perpendicular) * v_mod * (ran() * e_perp_1 + ran() * e_perp_2);
+
+		return dvdt;
+	}
+
 };
   
 #endif // COLLISIONS_HPP
--- tests/sd.cpp
+++ tests/sd.cpp
@@ -10,16 +10,16 @@ const double v0 = 1.84142e7; // m/s  (3.54 MeV of a proton)
 // const double pre_eta = 806039; // m^6 s^-4
 const double n0 = 1e20; // m
 const double Omega = 0.00085; // s^-1
-const double tau = 93e-6; // s
+const double tau = 93e-3; // s
 const double a = 0.5; // m
 
 // adimensional constants 
-const double gam = 3.42504e3;
-const double eta = 1.20055;
+const double gam = 3.42504e6;
+const double eta = 1200.55;
 
 // campo de temperatura (en realidad da la velocidad media en relación a v0)
 double Tf(const vector_type& v, double t){
-	return 0.053;
+	return 1.61029;
 }
 
 // campo de densidad (en relación a n0)
@@ -28,7 +28,7 @@ double nf(const vector_type& v, double t){
 }
 
 	/* Probando el frenamiento de un ion (un protón) por electrones en un plasma
-	** con densidad 10^20 1/m³ y temperatura 10 keV	
+	** con densidad 10^20 1/m³ y temperatura 2.5 keV	
 	** que es igual a una velocidad 978702 m/s = 0.053 v0
 	*/
 
@@ -41,10 +41,12 @@ vector<scalar_field_type> n = {nf};
 int q_a = 1;
 double m_a = 1836;
 
-Collisions col(q, m, loglambda, T, n, eta, m_a, q_a);
+NormalRand ran(2LL);
+
+Collisions col(q, m, loglambda, T, n, eta, m_a, q_a, ran);
 
 vector_type f(const state_type& x, double t){
-	return col.slow_down(x, 0);
+	return col.slow_down(x, 0) + col.dispersion(x, 0);
 }
 
 // Observer Example
@@ -72,11 +74,10 @@ int main(int argc, char* argv[]){
 	cout << f(x, 0);
 
 	ofstream fo("sldwn.dat");
-	ObserverVelocity obs(fo, 0.053);
+	ObserverVelocity obs(fo, 0.037581);
 
-	integrate(eq, x, 0, 0.2, 0.000001, obs);
-	// Should give 9.59919*10^-6, works better for smaller dt,
-	// maybe an adaptative dt will help
+	integrate(eq, x, 0, 1.2, 0.000001, obs);
+	// gives nice result for slowing down
 
 	cout << "Finished :)\n";
 
