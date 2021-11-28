0f1fdc8718f497dee9dff949c8cb65d0fb9ae784
Michel Romero <studentenherz@gmail.com>
2021-11-28 04:27:35 -0300

    .

--- tests/process_dispersion.cpp
+++ tests/process_dispersion.cpp
@@ -32,14 +32,18 @@ int main(int argc, char* argv[]){
 	// vector of velocities
 	vector<vector_type> vs;
 	double t, x, y, z, vx, vy, vz;
+	double t0 = -1, dt;
 	while(fi >> t >> x >> y >> z >> vx >> vy >> vz){
+		if(t0 == -1) t0 = t;
 		vs.push_back({vx, vy, vz});
 	}
 	int N = vs.size();
+	dt = (t - t0) / N;
 
 	// perp and par
 	vector<vector<double>> sqr_diffs_par(N);
 	vector<vector<double>> sqr_diffs_perp(N);
+	vector<vector<double>> v_mod(N);
 
 	vector_type v_sum, e_v, diff;
 	double diff_par, diff_perp;
@@ -58,18 +62,22 @@ int main(int argc, char* argv[]){
 
 			sqr_diffs_par[stop-start].push_back(sqr(diff_par));
 			sqr_diffs_perp[stop-start].push_back(sqr(diff_perp));
+			v_mod[stop-start].push_back(mod(v_sum) / (stop - start + 1)); // module of mean velocity
 		}
 	}
 
-	vector<double> mean_par, mean_perp;
+	vector<double> mean_par, mean_perp, mean_v_mod;
 
 	vector_mean(sqr_diffs_par, mean_par);
 	vector_mean(sqr_diffs_perp, mean_perp);
+	vector_mean(v_mod, mean_v_mod);
 
 	ofstream fo("mean_sqr_diff.dat");
 
+	cout << "Output to mean_sqr_diff.dat in the format:\nDt mean_Dv_par mea_Dv_perp mean_v\n";
+
 	for(int i=0; i<N; i++)
-		fo << mean_par[i] << ' ' << mean_perp[i] << '\n';
+		fo << dt * i << ' ' << mean_par[i] << ' ' << mean_perp[i] << ' ' << mean_v_mod[i] << '\n';
 
 	return 0;
 }
